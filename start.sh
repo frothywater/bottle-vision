b5c5d9105bbe893f147cc328e0673ad7acb1d2dc
frothywater <kokoroic@outlook.com>
2025-03-15 11:59:11 +0900

    train: Continue 3 tasks

--- start.sh
+++ start.sh
@@ -29,7 +29,7 @@ TB_PID=$!
 
 echo "Starting training..."
 # Run the training script (assumed to be repo/train.py)
-python main.py fit --config config/test.yaml --model.weight_path ../weights/organized-tag=512-char=128.ckpt
+python main.py fit --config config/test.yaml --model.weight_path ../weights/organized-tag=512-char=128-art=128.ckpt
 TRAIN_EXIT_CODE=$?
 
 echo "Training finished with exit code $TRAIN_EXIT_CODE. Stopping pod..."
